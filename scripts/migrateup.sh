19615440a5eedddbb8794953373ce130257a68b3
Jaspyy67 <2192885@bcu.edu.ph>
2025-03-11 10:06:29 +0800

    made changes to script

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Load variables from .env file if it exists
 if [ -f .env ]; then
     source .env
 fi
 
+# Change directory to migrations folder
 cd sql/schema
+
+# Run Goose migrations directly using DATABASE_URL
 goose turso $DATABASE_URL up
